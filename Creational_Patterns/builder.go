bfaee015de93ff5fe5a11c9bb1a087fdb5257dbd
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-01-01 19:54:27 +0530

    Builder Design Pattern in GoLang

---
+++ Creational_Patterns/builder.go
@@ -0,0 +1,59 @@
+package builder
+
+import "fmt"
+
+type Product struct {
+    Part1 string
+    Part2 string
+    Part3 string
+}
+
+type Builder interface {
+    BuildPart1()
+    BuildPart2()
+    BuildPart3()
+    GetProduct() Product
+}
+
+type ConcreteBuilder struct {
+    product Product
+}
+
+func (b *ConcreteBuilder) BuildPart1() {
+    b.product.Part1 = "Part 1 built"
+}
+
+func (b *ConcreteBuilder) BuildPart2() {
+    b.product.Part2 = "Part 2 built"
+}
+
+func (b *ConcreteBuilder) BuildPart3() {
+    b.product.Part3 = "Part 3 built"
+}
+
+func (b *ConcreteBuilder) GetProduct() Product {
+    return b.product
+}
+
+type Director struct {
+    builder Builder
+}
+
+func (d *Director) Construct() Product {
+    d.builder.BuildPart1()
+    d.builder.BuildPart2()
+    d.builder.BuildPart3()
+    return d.builder.GetProduct()
+}
+
+func Start() {
+    builder := &ConcreteBuilder{}
+
+    director := &Director{builder}
+
+    product := director.Construct()
+
+    finalProduct := builder.GetProduct()
+
+    fmt.Println("Product built:", finalProduct)
+}
